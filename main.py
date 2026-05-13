4c69eceb965e24b6c4beee6b23928f1ae1fb4911
syndiajean <syndiajean1@gmail.com>
2026-05-13 15:37:46 -0400

    First commit: main v1 with addition and multiplication

---
+++ main.py
@@ -0,0 +1,18 @@
+def main_simple(a, b, c):
+    return a * b + c
+
+calcul = main_simple(2, 4, 5)
+print(calcul)
+
+total = 0
+def somme(a, b):
+    global total
+    total = a + b
+    print(total)
+
+def multiplication(c):
+    result = c * total
+    return result
+
+somme(5, 10)
+print(multiplication(3))
